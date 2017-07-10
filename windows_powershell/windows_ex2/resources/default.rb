38645abe1a8430fe0b74c565e6ed0f083b63312f
flyingspur <seenu.ramachandran@gmail.com>
2017-07-10 15:12:54 +0530

    change in verbiage

--- windows_powershell/windows_ex2/resources/default.rb
+++ windows_powershell/windows_ex2/resources/default.rb
@@ -5,44 +5,44 @@ property :webappname, String
 
 action :create do
     for feature in ["Web-Server","Web-Http-Redirect","Web-Asp-Net45","Web-Log-Libraries","Web-Http-Tracing","Web-Custom-Logging","Web-Basic-Auth","Web-Mgmt-Service","Web-Mgmt-Console","File-Services"] do
-        powershell_script 'Install Web Server features' do
+        powershell_script "Install Web Server features - #{feature}" do
             code "Add-WindowsFeature #{feature}"
             not_if "(Get-WindowsFeature -Name #{feature}).Installed"
         end
     end
 
-    directory "#{webroot}/#{webappname}"
+    directory "#{new_resource.webroot}/#{new_resource.webappname}"
 
-    powershell_script "Create Application Pool - #{apppoolname}" do
+    powershell_script "Create Application Pool - #{new_resource.apppoolname}" do
         code <<-EOH
             Import-Module WebAdministration
-            New-Item -Path "IIS:/AppPools/#{apppoolname}"
+            New-Item -Path "IIS:/AppPools/#{new_resource.apppoolname}"
             EOH
         not_if <<-EOH
             Import-Module WebAdministration
-            Test-Path IIS:/AppPools/#{apppoolname} -pathType container
+            Test-Path IIS:/AppPools/#{new_resource.apppoolname} -pathType container
             EOH
     end
 
-    powershell_script "Application Pool - managedRuntimeVersion - #{apppoolname}" do
+    powershell_script "Application Pool - managedRuntimeVersion - #{new_resource.apppoolname}" do
         code <<-EOH
             Import-Module WebAdministration
-            Set-ItemProperty "IIS:/AppPools/#{apppoolname}" -Name "managedRuntimeVersion" -Value "v4.0"
+            Set-ItemProperty "IIS:/AppPools/#{new_resource.apppoolname}" -Name "managedRuntimeVersion" -Value "v4.0"
             EOH
         not_if <<-EOH
             Import-Module WebAdministration
-            (Get-ItemProperty IIS:/AppPools/#{apppoolname} -Name "managedRuntimeVersion") -ceq "v4.0"
+            (Get-ItemProperty IIS:/AppPools/#{new_resource.apppoolname} -Name "managedRuntimeVersion") -ceq "v4.0"
             EOH
     end
 
-    powershell_script "Application Pool - managedPipelineMode - #{apppoolname}" do
+    powershell_script "Application Pool - managedPipelineMode - #{new_resource.apppoolname}" do
         code <<-EOH
             Import-Module WebAdministration
-            Set-ItemProperty "IIS:/AppPools/#{apppoolname}" -Name "managedPipelineMode" -Value 1
+            Set-ItemProperty "IIS:/AppPools/#{new_resource.apppoolname}" -Name "managedPipelineMode" -Value 1
             EOH
         not_if <<-EOH
             Import-Module WebAdministration
-            (Get-ItemProperty IIS:/AppPools/#{apppoolname} -Name "managedPipelineMode") -ceq "Classic"
+            (Get-ItemProperty IIS:/AppPools/#{new_resource.apppoolname} -Name "managedPipelineMode") -ceq "Classic"
             EOH
     end
 
@@ -57,26 +57,26 @@ action :create do
             EOH
     end
 
-    powershell_script "Create IIS Web Application - #{webappname}" do
+    powershell_script "Create IIS Web Application - #{new_resource.webappname}" do
         code <<-EOH
             Import-Module WebAdministration
-            New-WebApplication -Name "#{webappname}" -ApplicationPool "#{apppoolname}" -Site "Root" -PhysicalPath 'C:/inetpub/#{webappname}'
+            New-WebApplication -Name "#{new_resource.webappname}" -ApplicationPool "#{new_resource.apppoolname}" -Site "Root" -PhysicalPath 'C:/inetpub/#{new_resource.webappname}'
             EOH
         not_if <<-EOH
             Import-Module WebAdministration
-            Test-Path "IIS:/Sites/Root/#{webappname}" -pathType container
+            Test-Path "IIS:/Sites/Root/#{new_resource.webappname}" -pathType container
             EOH
     end
 
-    for iis_node in ["Root", "Root/#{webappname}"] do
+    for iis_node in ["Root", "Root/#{new_resource.webappname}"] do
         powershell_script 'Set Application Pool' do
             code <<-EOH
                 Import-Module WebAdministration
-                Set-ItemProperty "IIS:/Sites/#{iis_node}" -Name "applicationPool" -Value "#{apppoolname}"
+                Set-ItemProperty "IIS:/Sites/#{iis_node}" -Name "applicationPool" -Value "#{new_resource.apppoolname}"
                 EOH
             not_if <<-EOH
                 Import-Module WebAdministration
-                (Get-ItemProperty "IIS:/Sites/#{iis_node}" -Name "applicationPool") -ceq "#{apppoolname}"
+                (Get-ItemProperty "IIS:/Sites/#{iis_node}" -Name "applicationPool") -ceq "#{new_resource.apppoolname}"
                 EOH
         end
     end
